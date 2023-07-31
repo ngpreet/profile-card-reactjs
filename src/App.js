ca60797e19bcc1f1c0d6dcaf89b9f4e5887b871b
ngpreet <ng.navpreet@gmail.com>
2023-07-31 09:54:06 +0530

    ui updates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Avatar />
+      <Info />
+      <Skills />
+    </div>
+  );
+}
+
+function Avatar() {
+  return (
+    <div className="avatar">
+      <img src="./navpreet.jpeg" alt="Navpreet Singh" />
+    </div>
+  );
+}
+
+function Info() {
+  return (
+    <div>
+      <h3>Navpreet Singh</h3>
+      <p>
+        Full-stack developer working at Infosys India. When not coding, I like
+        to play board games, and travel and experience new places.
+      </p>
+    </div>
+  );
+}
+
+function Skills() {
+  const skillsData = [
+    { name: "Javascript", bgColor: "#ACFADF", emoji: "💪" },
+    { name: "Angular", bgColor: "#CECE5A", emoji: "💪" },
+    { name: "HTML + CSS", bgColor: "#91C8E4", emoji: "💪" },
+    { name: "NodeJs", bgColor: "#A1CCD1", emoji: "💪" },
+    { name: "React", bgColor: "#FFD1DA", emoji: "👶" },
+  ];
+
+  return (
+    <div className="skill-container">
+      {skillsData.map((skill) => (
+        <SkillBadge name={skill.name} bgColor={skill.bgColor} emoji={skill.emoji} />
+      ))}
+    </div>
+  );
+}
+
+function SkillBadge(props) {
+  return (
+    <div className="skill-badge" style={{ background : props.bgColor}}>
+      {props.name} {props.emoji}
     </div>
   );
 }
