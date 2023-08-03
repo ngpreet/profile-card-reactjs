18010236452eb60cfb4e48c1a5f42e6a66c6e315
ngpreet <ng.navpreet@gmail.com>
2023-08-03 09:56:52 +0530

    code refactoring

--- src/App.js
+++ src/App.js
@@ -32,27 +32,42 @@ function Info() {
 
 function Skills() {
   const skillsData = [
-    { name: "Javascript", bgColor: "#ACFADF", emoji: "💪" },
-    { name: "Angular", bgColor: "#CECE5A", emoji: "💪" },
-    { name: "HTML + CSS", bgColor: "#91C8E4", emoji: "💪" },
-    { name: "NodeJs", bgColor: "#A1CCD1", emoji: "💪" },
-    { name: "React", bgColor: "#FFD1DA", emoji: "👶" },
+    { name: "Javascript", bgColor: "#ACFADF", level: "advanced" },
+    { name: "Angular", bgColor: "#CECE5A", level: "advanced" },
+    { name: "HTML + CSS", bgColor: "#91C8E4", level: "advanced" },
+    { name: "NodeJs", bgColor: "#D8D9DA", level: "advanced" },
+    { name: "GIT + GitHub", bgColor: "#A084E8", level: "intermediate" },
+    { name: "React", bgColor: "#FFD1DA", level: "beginner" },
   ];
 
   return (
     <div className="skill-container">
       {skillsData.map((skill) => (
-        <SkillBadge name={skill.name} bgColor={skill.bgColor} emoji={skill.emoji} />
+        <SkillBadge
+          name={skill.name}
+          bgColor={skill.bgColor}
+          level={skill.level}
+        />
       ))}
     </div>
   );
 }
 
-function SkillBadge(props) {
+function SkillBadge({ name, bgColor, level }) {
   return (
-    <div className="skill-badge" style={{ background : props.bgColor}}>
-      {props.name} {props.emoji}
+    <div className="skill-badge" style={{ background: bgColor }}>
+      {name} <SkillLevelEmoji level={level} />
     </div>
+  );
+}
+
+function SkillLevelEmoji({ level }) {
+  return (
+    <span>
+      {level === "advanced" && "💪"}
+      {level === "intermediate" && "👍"}
+      {level === "beginner" && "👶"}
+    </span>
   );
 }
 
